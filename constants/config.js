e8c421a3be7af73679757eaf27a45a34ffb46e73
RIZKI <RIZKI@DESKTOP-V2QARS3.local>
2021-05-29 11:13:58 +0700

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,11 +1,5 @@
 const config = {
-    // url: 'http://10.5.248.212:8000',
-    //url: 'http://10.5.248.44/iym_app/API/iym_mobile/server-api/public',
-    // url: 'http://10.5.252.250/iym_mobile/API/iym_mobile/server-api/public',
-    // url: 'http://202.179.186.21:8000',
-    // url: 'http://10.5.252.116:8000',
-     url: 'https://www.smoebatam.com/API/iym_mobile/server-api/public',
-    // furl: 'https://www.smoebatam.com/API/iym_mobile/server-api/public',
+     url: 'marzdeveloper.club',
 };
 
 export default {
